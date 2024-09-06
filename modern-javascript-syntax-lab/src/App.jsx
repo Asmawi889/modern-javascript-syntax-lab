1a0e39d2a556b65a1d804963056905a9ae23cf99
YUSUF <Yousifcool889@gmail.com>
2024-09-06 13:57:45 +0300

    inital push

---
+++ modern-javascript-syntax-lab/src/App.jsx
@@ -0,0 +1,41 @@
+const App = () => {
+
+const array1 = ['vanilla', 'chocolate', 'strawberry'];
+
+const array2 = array1.map((currentElement) => {
+  return currentElement + ' ice cream';
+});
+
+console.log(array2); // ['vanilla ice cream', 'chocolate ice cream', 'strawberry ice cream']
+
+// Use `.map()` to iterate over the following array:
+
+const nums = [13, 87, 2, 89, 12, 4, 90, 63];
+
+// Create a new array where each value is multiplied by 2 and log the new array.
+
+// Your code here
+const addnumb = nums.map((number) => {
+  return number * 2
+})
+
+
+console.log(addnumb)
+
+
+// Given the following array, use destructuring to pull out the first and second values and place them into variables. Log both variables.
+
+const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+
+// Your code here
+
+
+
+
+
+
+
+
+
+}
+export default App
